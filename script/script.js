4c42bb37ba65bf6df7e29745cf559771dc281d2f
Alexandr <kuvaldin.ai95@gmail.com>
2020-10-20 17:21:50 +0700

    Выполнил ДЗ к 9-у уроку

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let money;
 
 let start = function (){
@@ -162,3 +162,54 @@ console.log(capitalize(c));
 };
 
 addExpensesLog();
+*/
+//Урок №9
+
+let moneyMonth = document.querySelector('.salary-amount');
+console.log('moneyMonth: ', moneyMonth);
+let incomeTitle = document.querySelector('.income-items .income-title');
+console.log('incomeTitle: ', incomeTitle);
+let incomeAmount = document.querySelector('.income-amount');
+console.log('incomeAmount: ', incomeAmount);
+
+let expensesTitle = document.querySelector('.expenses-items .expenses-title');
+console.log('expensesTitle: ', expensesTitle);
+let expensesAmount = document.querySelector('.expenses-amount');
+console.log('expensesAmount: ', expensesAmount);
+
+let additionalExpensesItem = document.querySelector('.additional_expenses-item');
+console.log('additionalExpensesItem: ', additionalExpensesItem);
+
+let targetAmount = document.querySelector('.target-amount');
+console.log('targetAmount: ', targetAmount);
+
+let periodSelect = document.querySelector('.period-select');
+console.log('periodSelect: ', periodSelect);
+
+let buttonStart = document.getElementById('start');
+console.log('buttonStart: ', buttonStart);
+
+let btnPlusIncome = document.getElementsByTagName('button')[0];
+console.log('btnPlusIncome: ', btnPlusIncome);
+let btnPlusExpenses = document.getElementsByTagName('button')[1];
+console.log('btnPlusExpenses: ', btnPlusExpenses);
+
+let depositCheck = document.querySelector('#deposit-check');
+console.log('depositCheck: ', depositCheck);
+
+let incomeItem = document.querySelectorAll('.additional_income-item');
+console.log('incomeItem: ', incomeItem);
+
+
+
+let budgetMonth = document.getElementsByClassName('budget_month-value');
+let budgetDay = document.getElementsByClassName('budget_day-value');
+let budgetExpenses = document.getElementsByClassName('expenses_month-value');
+let additionalIncome = document.getElementsByClassName('additional_income-value');
+let additionalExpenses = document.getElementsByClassName('additional_expenses-value');
+let incomePeriod = document.getElementsByClassName('income_period-value');
+let targetMonth = document.getElementsByClassName('target_month-value');
+
+
+
+
