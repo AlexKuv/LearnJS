7210ca34c3ba08eb4177e59364ed47f3f64fde1d
Alexandr <kuvaldin.ai95@gmail.com>
2020-11-06 02:48:50 +0700

    Квадрат теперь двигается всегда

--- script/script.js
+++ script/script.js
@@ -36,24 +36,72 @@ let newObj = new DomElement('.block', '100', '100','#A8E4A0');
 document.addEventListener('DOMContentLoaded', newObj.createDiv());
 
 
-document.addEventListener('keydown', (e) =>{
-  let square = document.querySelector('.block');
-  let left =0;
-  let top =30;
-  if(e.key === 'ArrowRight'){
-    left += 10;
-    square.style.left = left + 'px';
-  }
-  if (e.code === 'ArrowLeft') {
-     left -= 10;
-     square.style.left = left + 'px';
-  }
-  if (e.code === 'ArrowDown') {
-     top += 10;
-     square.style.top = top + 'px';
-  }
-  if (e.code === 'ArrowUp') {
-     top -= 10;
-     square.style.top = top + 'px';
-  }
-});
+	    let Square = function (node) {
+        this.x = 0;
+        this.y = 40;
+        this.step = 10;
+        this.node = node;
+        this.setHandlers();
+    };
+ 
+    Square.prototype.setHandlers = function () {
+        let keys = {
+            37: "left",
+            38: "up",
+            39: "right",
+            40: "down"
+        };
+        let that = this;
+        document.addEventListener("keydown", function (e) {
+            if (e.keyCode in keys) {
+                that[keys[e.keyCode]]();
+                that.draw();
+            }
+        }, false);
+    };
+ 
+    Square.prototype.left = function () {
+        this.x -= this.step;
+    };
+ 
+    Square.prototype.right = function () {
+        this.x += this.step;
+    };
+ 
+    Square.prototype.up = function () {
+        this.y -= this.step;
+    };
+ 
+    Square.prototype.down = function () {
+        this.y += this.step;
+    };
+ 
+    Square.prototype.draw = function () {
+        this.node.style.left = this.x + "px";
+        this.node.style.top = this.y + "px";
+    };
+ 
+    new Square(document.querySelector("div"));
+
+
+// document.addEventListener('keydown', (e) =>{
+//   let square = document.querySelector('.block');
+//   let left =0;
+//   let top =30;
+//   if(e.key === 'ArrowRight'){
+//     left += 10;
+//     square.style.left = left + 'px';
+//   }
+//   if (e.code === 'ArrowLeft') {
+//      left -= 10;
+//      square.style.left = left + 'px';
+//   }
+//   if (e.code === 'ArrowDown') {
+//      top += 10;
+//      square.style.top = top + 'px';
+//   }
+//   if (e.code === 'ArrowUp') {
+//      top -= 10;
+//      square.style.top = top + 'px';
+//   }
+// });
